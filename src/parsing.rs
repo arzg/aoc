34cec363aea0a68c7474894caf20e942b95cd340
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-10-21 14:58:02 +1100

    Add basic parsing library
    I copy-pasted this from https://github.com/arzg/eldiro/blob/f962c62db382a261281986eb800c0966c93ca522/crates/eldiro/src/utils.rs

---
+++ src/parsing.rs
@@ -0,0 +1,36 @@
+pub(crate) fn tag<'a, 'b>(starting_text: &'a str, s: &'b str) -> Result<&'b str, String> {
+    if s.starts_with(starting_text) {
+        Ok(&s[starting_text.len()..])
+    } else {
+        Err(format!("expected {}", starting_text))
+    }
+}
+
+pub(crate) fn extract_digits(s: &str) -> Result<(&str, &str), String> {
+    take_while1(|c| c.is_ascii_digit(), s, "expected digits".to_string())
+}
+
+fn take_while1(
+    accept: impl Fn(char) -> bool,
+    s: &str,
+    error_msg: String,
+) -> Result<(&str, &str), String> {
+    let (remainder, extracted) = take_while(accept, s);
+
+    if extracted.is_empty() {
+        Err(error_msg)
+    } else {
+        Ok((remainder, extracted))
+    }
+}
+
+fn take_while(accept: impl Fn(char) -> bool, s: &str) -> (&str, &str) {
+    let extracted_end = s
+        .char_indices()
+        .find_map(|(idx, c)| if accept(c) { None } else { Some(idx) })
+        .unwrap_or_else(|| s.len());
+
+    let extracted = &s[..extracted_end];
+    let remainder = &s[extracted_end..];
+    (remainder, extracted)
+}
