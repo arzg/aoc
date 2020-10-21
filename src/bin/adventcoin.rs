cb43949bcc03ea760040d55b2a4c92acc38e3f2e
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-10-21 21:17:47 +1100

    Complete 2015 Day 4 Part 2

--- src/bin/adventcoin.rs
+++ src/bin/adventcoin.rs
@@ -4,5 +4,7 @@ const SECRET_KEY: &str = "bgvyzdsv";
 
 fn main() {
     let mut miner = Miner::new(SECRET_KEY);
-    println!("{}", miner.mine(5));
+
+    println!("Lowest with 5 leading zeroes: {}", miner.mine(5));
+    println!("Lowest with 6 leading zeroes: {}", miner.mine(6));
 }
