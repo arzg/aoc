21198690b2dd35cf7bb1f6fdbe6c359a9b87c033
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-10-21 17:50:31 +1100

    Complete 2015 Day 3 Part 2

--- src/bin/houses.rs
+++ src/bin/houses.rs
@@ -4,5 +4,10 @@ const INPUT: &str = include_str!("houses_input");
 
 fn main() {
     let moves = Moves::new(INPUT.trim()).unwrap();
-    println!("{}", moves.num_houses_with_presents());
+
+    println!(
+        "without Robo-Santa: {}",
+        moves.num_houses_with_presents(false),
+    );
+    println!("with Robo-Santa: {}", moves.num_houses_with_presents(true));
 }
--- src/houses.rs
+++ src/houses.rs
@@ -19,15 +19,45 @@ impl Moves {
         Some(Self { moves })
     }
 
-    pub fn num_houses_with_presents(&self) -> usize {
-        let mut visited_houses = self.visited_houses();
+    pub fn num_houses_with_presents(&self, robosanta: bool) -> usize {
+        let mut visited_houses = if robosanta {
+            self.visited_houses_with_robosanta()
+        } else {
+            self.visited_houses_without_robosanta()
+        };
+
         visited_houses.sort_unstable();
         visited_houses.dedup();
 
         visited_houses.len()
     }
 
-    fn visited_houses(&self) -> Vec<House> {
+    fn visited_houses_with_robosanta(&self) -> Vec<House> {
+        let mut santa_pos = Coordinate { x: 0, y: 0 };
+        let mut robosanta_pos = Coordinate { x: 0, y: 0 };
+
+        let mut visited_houses = vec![House {
+            pos: Coordinate { x: 0, y: 0 },
+        }];
+
+        for (idx, move_) in self.moves.iter().enumerate() {
+            let pos_being_moved = if idx % 2 == 0 {
+                &mut santa_pos
+            } else {
+                &mut robosanta_pos
+            };
+
+            pos_being_moved.apply_move(*move_);
+
+            visited_houses.push(House {
+                pos: *pos_being_moved,
+            });
+        }
+
+        visited_houses
+    }
+
+    fn visited_houses_without_robosanta(&self) -> Vec<House> {
         let mut current_pos = Coordinate { x: 0, y: 0 };
         let mut visited_houses = vec![House { pos: current_pos }];
 
@@ -71,14 +101,43 @@ impl Coordinate {
 }
 
 #[cfg(test)]
-mod tests {
+mod with_robosanta_tests {
     use super::*;
 
     fn check(input: &str, expected_num_houses_with_presents: usize) {
         let moves = Moves::new(input).unwrap();
 
         assert_eq!(
-            moves.num_houses_with_presents(),
+            moves.num_houses_with_presents(true),
+            expected_num_houses_with_presents,
+        );
+    }
+
+    #[test]
+    fn up_down() {
+        check("^v", 3);
+    }
+
+    #[test]
+    fn up_right_down_left() {
+        check("^<v>", 3);
+    }
+
+    #[test]
+    fn up_down_up_down_up_down() {
+        check("^v^v^v", 7);
+    }
+}
+
+#[cfg(test)]
+mod without_robosanta_tests {
+    use super::*;
+
+    fn check(input: &str, expected_num_houses_with_presents: usize) {
+        let moves = Moves::new(input).unwrap();
+
+        assert_eq!(
+            moves.num_houses_with_presents(false),
             expected_num_houses_with_presents,
         );
     }
