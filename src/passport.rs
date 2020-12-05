dee32d12a087eb89ee68c2b6b74887383b40e8af
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-05 17:27:15 +1100

    Avoid allocation of passport fields

--- src/passport.rs
+++ src/passport.rs
@@ -1,18 +1,18 @@
 use std::collections::HashMap;
 
-pub struct Passport {
-    birth_year: Option<String>,
-    issue_year: Option<String>,
-    expiration_year: Option<String>,
-    height: Option<String>,
-    hair_color: Option<String>,
-    eye_color: Option<String>,
-    passport_id: Option<String>,
+pub struct Passport<'a> {
+    birth_year: Option<&'a str>,
+    issue_year: Option<&'a str>,
+    expiration_year: Option<&'a str>,
+    height: Option<&'a str>,
+    hair_color: Option<&'a str>,
+    eye_color: Option<&'a str>,
+    passport_id: Option<&'a str>,
     #[allow(dead_code)]
-    country_id: Option<String>,
+    country_id: Option<&'a str>,
 }
 
-impl Passport {
+impl Passport<'_> {
     pub fn is_complete(&self) -> bool {
         self.birth_year.is_some()
             && self.issue_year.is_some()
@@ -25,17 +25,14 @@ impl Passport {
 
     pub fn is_valid(&self) -> bool {
         self.birth_year
-            .as_ref()
             .map_or(false, |byr| (1920..=2002).contains(&byr.parse().unwrap()))
             && self
                 .issue_year
-                .as_ref()
                 .map_or(false, |iyr| (2010..=2020).contains(&iyr.parse().unwrap()))
             && self
                 .expiration_year
-                .as_ref()
                 .map_or(false, |eyr| (2020..=2030).contains(&eyr.parse().unwrap()))
-            && self.height.as_ref().map_or(false, |hgt| {
+            && self.height.map_or(false, |hgt| {
                 hgt.strip_suffix("cm").map_or_else(
                     || {
                         hgt.strip_suffix("in")
@@ -44,41 +41,41 @@ impl Passport {
                     |cm| (150..=193).contains(&cm.parse().unwrap()),
                 )
             })
-            && self.hair_color.as_ref().map_or(false, |hcl| {
+            && self.hair_color.map_or(false, |hcl| {
                 hcl.strip_prefix('#').map_or(false, |hex| {
                     hex.len() == 6 && hex.chars().all(|c| c.is_ascii_hexdigit())
                 })
             })
-            && self.eye_color.as_ref().map_or(false, |ecl| {
-                ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&ecl.as_str())
+            && self.eye_color.map_or(false, |ecl| -> bool {
+                ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&ecl)
             })
-            && self.passport_id.as_ref().map_or(false, |pid| {
+            && self.passport_id.map_or(false, |pid| {
                 pid.len() == 9 && pid.chars().all(|c| c.is_ascii_digit())
             })
     }
 }
 
-impl From<&str> for Passport {
-    fn from(s: &str) -> Self {
-        let fields: HashMap<String, String> = s
+impl<'a> From<&'a str> for Passport<'a> {
+    fn from(s: &'a str) -> Self {
+        let fields: HashMap<_, _> = s
             .split_whitespace()
             .map(|field| {
                 let colon_idx = field.find(':').unwrap();
                 let (field_name, field_value) = (&field[..colon_idx], &field[colon_idx + 1..]);
 
-                (field_name.to_string(), field_value.to_string())
+                (field_name, field_value)
             })
             .collect();
 
         Passport {
-            birth_year: fields.get("byr").cloned(),
-            issue_year: fields.get("iyr").cloned(),
-            expiration_year: fields.get("eyr").cloned(),
-            height: fields.get("hgt").cloned(),
-            hair_color: fields.get("hcl").cloned(),
-            eye_color: fields.get("ecl").cloned(),
-            passport_id: fields.get("pid").cloned(),
-            country_id: fields.get("cid").cloned(),
+            birth_year: fields.get("byr").copied(),
+            issue_year: fields.get("iyr").copied(),
+            expiration_year: fields.get("eyr").copied(),
+            height: fields.get("hgt").copied(),
+            hair_color: fields.get("hcl").copied(),
+            eye_color: fields.get("ecl").copied(),
+            passport_id: fields.get("pid").copied(),
+            country_id: fields.get("cid").copied(),
         }
     }
 }
