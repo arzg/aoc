fc232f5f79f79d374fe37f50f4aa4c311a931a49
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-05 17:16:10 +1100

    Complete 2020 Day 4 Part 2

--- src/bin/passport.rs
+++ src/bin/passport.rs
@@ -3,11 +3,18 @@ use aoc::passport::Passport;
 const INPUT: &str = include_str!("passport_input");
 
 fn main() {
-    let num_valid_passports = INPUT
-        .split("\n\n")
-        .map(Passport::from)
+    let passports: Vec<_> = INPUT.split("\n\n").map(Passport::from).collect();
+
+    let num_complete_passports = passports
+        .iter()
+        .filter(|passport| passport.is_complete())
+        .count();
+
+    let num_valid_passports = passports
+        .iter()
         .filter(|passport| passport.is_valid())
         .count();
 
-    println!("{}", num_valid_passports);
+    println!("Number of complete passports: {}", num_complete_passports);
+    println!("Number of valid passports: {}", num_valid_passports);
 }
--- src/passport.rs
+++ src/passport.rs
@@ -5,7 +5,7 @@ pub struct Passport {
 }
 
 impl Passport {
-    pub fn is_valid(&self) -> bool {
+    pub fn is_complete(&self) -> bool {
         self.fields.contains_key("byr")
             && self.fields.contains_key("iyr")
             && self.fields.contains_key("eyr")
@@ -13,6 +13,40 @@ impl Passport {
             && self.fields.contains_key("hcl")
             && self.fields.contains_key("ecl")
             && self.fields.contains_key("pid")
+    }
+
+    pub fn is_valid(&self) -> bool {
+        self.fields
+            .get("byr")
+            .map_or(false, |byr| (1920..=2002).contains(&byr.parse().unwrap()))
+            && self
+                .fields
+                .get("iyr")
+                .map_or(false, |iyr| (2010..=2020).contains(&iyr.parse().unwrap()))
+            && self
+                .fields
+                .get("eyr")
+                .map_or(false, |eyr| (2020..=2030).contains(&eyr.parse().unwrap()))
+            && self.fields.get("hgt").map_or(false, |hgt| {
+                hgt.strip_suffix("cm").map_or_else(
+                    || {
+                        hgt.strip_suffix("in")
+                            .map_or(false, |in_| (59..=76).contains(&in_.parse().unwrap()))
+                    },
+                    |cm| (150..=193).contains(&cm.parse().unwrap()),
+                )
+            })
+            && self.fields.get("hcl").map_or(false, |hcl| {
+                hcl.strip_prefix('#').map_or(false, |hex| {
+                    hex.len() == 6 && hex.chars().all(|c| c.is_ascii_hexdigit())
+                })
+            })
+            && self.fields.get("ecl").map_or(false, |ecl| {
+                ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&ecl.as_str())
+            })
+            && self.fields.get("pid").map_or(false, |pid| {
+                pid.len() == 9 && pid.chars().all(|c| c.is_ascii_digit())
+            })
     }
 }
 
