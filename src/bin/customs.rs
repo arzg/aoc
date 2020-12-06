51cc4008937670024dc6c03da58654209b1f9505
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-06 16:13:29 +1100

    Complete 2020 Day 6 Part 2

--- src/bin/customs.rs
+++ src/bin/customs.rs
@@ -1,7 +1,7 @@
 const INPUT: &str = include_str!("customs_input");
 
 fn main() {
-    let num_yes_answers_per_group: usize = INPUT
+    let num_yes_anyone_in_group: usize = INPUT
         .split("\n\n")
         .map(|group| {
             let mut answers_in_group: Vec<_> = group.lines().flat_map(str::chars).collect();
@@ -12,5 +12,37 @@ fn main() {
         })
         .sum();
 
-    println!("{}", num_yes_answers_per_group);
+    println!(
+        "Number of questions to which anyone in a group answered ‘yes’: {}",
+        num_yes_anyone_in_group,
+    );
+
+    let num_yes_everyone_in_group: usize = INPUT
+        .split("\n\n")
+        .map(|group| {
+            let mut answers_in_group: Vec<_> = group.lines().flat_map(str::chars).collect();
+            answers_in_group.sort();
+            answers_in_group.dedup();
+
+            let group_answers: Vec<Vec<_>> =
+                group.lines().map(str::chars).map(|c| c.collect()).collect();
+
+            let mut num_yes_everyone_in_group = 0;
+            for answer in answers_in_group {
+                if group_answers
+                    .iter()
+                    .all(|answers| answers.contains(&answer))
+                {
+                    num_yes_everyone_in_group += 1;
+                }
+            }
+
+            num_yes_everyone_in_group
+        })
+        .sum();
+
+    println!(
+        "Number of questions to which everyone in a group answered ‘yes’: {}",
+        num_yes_everyone_in_group,
+    );
 }
