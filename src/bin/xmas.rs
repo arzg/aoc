cc7d51c903773b602943059f9edc41ce15d7c821
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-09 16:14:09 +1100

    Complete 2020 Day 9 Part 2

--- src/bin/xmas.rs
+++ src/bin/xmas.rs
@@ -4,5 +4,7 @@ const INPUT: &str = include_str!("xmas_input");
 
 fn main() {
     let input = Input::new(INPUT);
-    println!("{}", input.first_invalid_number());
+
+    println!("Smallest invalid number: {}", input.first_invalid_number());
+    println!("Encryption weakness: {}", input.determine_weakness());
 }
--- src/xmas.rs
+++ src/xmas.rs
@@ -9,6 +9,25 @@ impl Input {
         }
     }
 
+    pub fn determine_weakness(&self) -> u64 {
+        let first_invalid_number = self.first_invalid_number();
+
+        for start_idx in 0..self.data.len() {
+            for end_idx in start_idx + 2..self.data.len() {
+                let range = &self.data[start_idx..end_idx];
+
+                if range.iter().sum::<u64>() == first_invalid_number {
+                    let smallest_in_range = range.iter().min().unwrap();
+                    let largest_in_range = range.iter().max().unwrap();
+
+                    return smallest_in_range + largest_in_range;
+                }
+            }
+        }
+
+        unreachable!()
+    }
+
     pub fn first_invalid_number(&self) -> u64 {
         'data: for (idx, current) in self.data.iter().copied().enumerate().skip(25) {
             let last_25_numbers = &self.data[idx - 25..idx];
